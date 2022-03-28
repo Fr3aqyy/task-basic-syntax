29540cd75aaded116fbb39549249aafe3bf72569
Ivan <lii190601@gmail.com>
2022-03-28 17:19:41 +0300

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,14 @@
 export function romanToInteger(str) {
+    const valueRoman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let prevValue = valueRoman[str[0]];
+    for (let romanNumber of str) {
+        let value = valueRoman[romanNumber];
+        result += value;
+        if (prevValue < value) {
+            result -= prevValue * 2;
+        }
+        prevValue = value;
+    }
     return result;
 }
